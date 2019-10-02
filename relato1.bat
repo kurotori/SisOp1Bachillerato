66190cb790672e872a572d275580c100d20c92b5
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2019-10-02 13:37:04 -0300

    Actualización del código

--- relato1.bat
+++ relato1.bat
@@ -8,6 +8,7 @@ set /p nombre2=Escribe otro nombre:
 set /p cosa1=Escribe el nombre de un objeto, con su articulo (un ..., una ...):
 set /p cosa2=Escribe el nombre de otro objeto, con su articulo (un ..., una ...):
 set /p cosa3=Escribe el nombre de otro objeto mas, con su articulo (un ..., una ...):
+set /p cosa4=Escribe el nombre de otro objeto mas, con su articulo (un ..., una ...):
 set /p verbo1=Escribe un verbo en inifinitivo (-ar,-er,-ir):
 set /p verbo2=Escribe otro verbo en inifinitivo (-ar,-er,-ir):
 set /p verbo3=Escribe un gerundio (-ando,-endo):
@@ -20,6 +21,6 @@ echo Un dia, %nombre1% estaba por ir a %verbo2% en %lugar2%. >> %numrelato%.txt
 echo En ese momento vio %cosa3% %verbo3% por %lugar1%. >> %numrelato%.txt
 echo Como no podia ir a %verbo2% con %cosa3% %verbo3%, busco %cosa1% >> %numrelato%.txt
 echo y llamo a %nombre2% para que lo viniera a %verbo1% con %cosa2%. >> %numrelato%.txt
-echo Pero %nombre2% estaba viendo %verbo4% en %lugar3% y no le hizo caso. >> %numrelato%.txt
+echo Pero %nombre2% estaba viendo %verbo4% %cosa4% en %lugar3% y no le hizo caso. >> %numrelato%.txt
 
 notepad %numrelato%.txt
